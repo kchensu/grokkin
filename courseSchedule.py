479bab121e252e1004a98d473fd8a7eabf37d724
kchensu <kchensu@sfu.ca>
2022-03-16 00:09:50 -0700

    merge intervals

--- courseSchedule.py
+++ courseSchedule.py
@@ -27,5 +27,5 @@ def canFinish(numCourses, prerequisites):
 
 numCourses = 2
 prerequisites = [[1, 0]]
-prerequisites = [[1, 0], [0, 1]]
+# prerequisites = [[1, 0], [0, 1]]
 print(canFinish(numCourses, prerequisites))
